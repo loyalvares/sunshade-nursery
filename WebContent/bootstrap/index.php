7b158f20de14183fba400079aa3a506abad1d45a
Loy Alvares <loyalvares@hotmail.com>
2017-03-20 22:14:21 +0530

    First Commit!

---
+++ WebContent/bootstrap/index.php
@@ -0,0 +1,384 @@
+<!DOCTYPE html>
+
+<html lang="en-us">
+
+<head>
+	<title>Mangalore Nursery, Mangalore - Indoor and Fruit Plants, Seeds Supplier</title>
+	
+	<meta name="author" content="Mangalore Nursery">
+	
+	<meta name="description" content="If you have a dream of gardening, if you are a fond of collecting valuable and ornamental plants for your premises, we will provide you the best budget solution, keeping and maintaining the outstanding qualities and consultancy services.">
+	
+	<meta name="keywords" content="Indian Plants, Flowering Plants, Fruit Plants, Hanging Plants, Herbal Plants, Indoor Plants, Palm Plants, Aquatic Plants, Cactus Plants">
+
+	<meta charset="utf-8" >
+
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<title></title>
+	
+	<!-- CSS files -->
+
+	<link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:100,300,400,600,700,900,400italic|Montserrat:400,700' rel='stylesheet'>
+
+	<link rel="stylesheet" href="plugins/bootstrap/css/bootstrap.min.css">
+
+	<link rel="stylesheet" href="plugins/font-awesome/css/font-awesome.min.css">
+
+	<link rel="stylesheet" href="plugins/elegant_font/html_css/style.css">
+
+	<link rel="stylesheet" href="plugins/rs-plugin/css/settings.css" media="screen">
+
+	<link rel="stylesheet" href="plugins/owl-carousel/owl.carousel.css">
+
+	<!-- Main CSS file -->
+
+	<link rel="stylesheet" href="css/style.css">
+	
+</head>
+
+<body>
+	
+	<div id="preloader">
+		<div id="status">&nbsp;</div>
+	</div>
+	
+	
+	<!-- Global Wrapper -->
+	<div id="wrapper">
+	
+		<!-- Add gray class to make the topbar light -->
+		<!-- Top Bar -->
+		<div class="topbar gray">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-6 hidden-xs">
+						<ul class="social-icon unstyled">
+							<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+							<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+							<li><a href="#"><i class="fa fa-instagram"></i></a></li>
+						</ul>
+					</div>
+					<div class="col-sm-6">
+						<div class="pull-right">
+							<ul class="top-menu">
+								<li><a href="#"><i class="fa fa-lock"></i>  Sign In</a></li>
+								<li><span class="py10"> | </span></li>
+								<li><a href="#"><i class="fa fa-shopping-cart"></i> Cart <span class="cart-items"></span></a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+	
+		<!-- Header -->
+		<header class="header-wrapper">
+			<div class="main-header">
+				<div class="container">
+					<div class="row">
+						<div class="col-sm-12 col-md-2">
+							<!-- Logo - Read documentation to see how to change the logo-->
+							<a href="index.php" class="logo"></a>
+						</div>
+						<div class="col-sm-12 col-md-10">
+							<nav class="navbar-left">
+								<!-- Added menu-light class to make the menu white -->
+								<ul class="menu menu-light">
+									<!-- Toggle Menu - For Mobile Devices -->
+									<li class="toggle-menu xs-toggle"><i class="fa icon_menu"></i></li>
+									
+									<li class="first">
+										<a href="#" class="first"><i class="fa fa-home menu-icon"></i> Home</a>
+									</li> <!-- END Home -->
+									<li>
+										<a href="#">About Us</a>
+									</li> <!-- END About Us -->
+									<li>
+										<a href="#">Services</a>
+									</li> <!-- END Services -->
+									<li>
+										<a href="#">Profile</a>
+									</li> <!-- END Profile -->
+									<li>
+										<a href="#">Offer</a>
+									</li> <!-- END Offer -->
+									<li>
+										<a href="#">Contact Us</a>
+									</li> <!-- END Contact Us -->									
+								</ul>
+							</nav>
+						</div>
+					</div>
+				</div> <!-- END Container -->
+			</div> <!-- END Main Header -->
+		</header>
+	</div>
+
+	<div class="tp-banner-container rs_fullwidth">
+		<div class="tp-banner">
+			<ul>
+				
+				<!-- SLIDE 1 -->
+				<li data-transition="fade" data-masterspeed="500" data-slotamount="7" data-delay="8000" data-title="Slide 1">
+					<!-- Background Image -->
+				   <img src="images/carousel/slide_1.jpg" data-bgposition="center top" data-bgfit="cover" data-bgrepeat="no-repeat" alt="">
+				   <!-- <div class="bg-overlay op6"></div> -->
+
+
+					<div class="tp-caption sfb stt"
+						data-x="left" data-hoffset="15"
+						data-y="center" data-voffset="-75" 
+						data-speed="600" data-start="600"
+						data-endspeed="400" data-end="7900"
+						data-easing="Power3.easeInOut" data-endeasing=""
+						style="z-index: 4">
+						<h3 class="title-slider-small uppercased mb10">create stunning websites with palas</h3>
+					</div>
+
+					<div class="tp-caption sfb stt"
+						data-x="left" data-hoffset="15"
+						data-y="center" data-voffset="25" 
+						data-speed="700" data-start="800"
+						data-endspeed="500" data-end="7950"
+						data-easing="Power3.easeInOut" data-endeasing=""
+						style="z-index: 4">
+						<h2 class="title-slider-large">All you need<br> just one template</h2>
+					</div>
+
+					<div class="tp-caption sfb stt"
+						data-x="left" data-hoffset="15"
+						data-y="center" data-voffset="130" 
+						data-speed="800" data-start="1000"
+						data-endspeed="600" data-end="8000"
+						data-easing="Power3.easeInOut" data-endeasing=""
+						style="z-index: 4">
+						<a href="#" class="btn-slider">Purchase</a>
+					</div>
+
+					
+				</li>
+
+				<!-- SLIDE 2 -->
+				<li data-transition="fade" data-slotamount="7" data-delay="8000" data-title="Slide 2">
+					<!-- Background Image -->
+				   <img src="images/carousel/slide_2.jpg" data-bgposition="center top" data-bgfit="cover" data-bgrepeat="no-repeat" alt="">
+				   <!-- <div class="bg-overlay op5"></div> -->
+
+				   
+					<div class="tp-caption sfl stt"
+						data-x="left" data-hoffset="15"
+						data-y="center" data-voffset="-10" 
+						data-speed="600" data-start="600"
+						data-endspeed="400" data-end="7800"
+						data-easing="Power3.easeInOut" data-endeasing=""
+						style="z-index: 4">
+						<h2 class="title-slider-large mb20">Customizable <br> and Easy to Use</h2>
+						<div class="br-bottom"></div>
+					</div>
+
+					<div class="tp-caption sfl stb"
+						data-x="left" data-hoffset="15"
+						data-y="center" data-voffset="110" 
+						data-speed="700" data-start="800"
+						data-endspeed="400" data-end="7900"
+						data-easing="Power3.easeInOut" data-endeasing=""
+						style="z-index: 4">
+						<h4 class="title-slider-small">It could take you a few minutes to customize it!</h4>
+					</div>
+					
+				</li>
+
+				<!-- SLIDE 3 -->
+				<li data-transition="fade" data-slotamount="7" data-delay="8000" data-title="Slide 3">
+					<!-- Background Image -->
+				   <img src="images/carousel/slide_3.jpg" data-bgposition="center top" data-bgfit="cover" data-bgrepeat="no-repeat" alt="">
+				   <!-- <div class="bg-overlay op5"></div> -->
+
+				   
+					<div class="tp-caption sfl stl"
+						data-x="left" data-hoffset="15"
+						data-y="center" data-voffset="-15" 
+						data-speed="700" data-start="600"
+						data-endspeed="400" data-end="7800"
+						data-easing="Power3.easeInOut" data-endeasing=""
+						style="z-index: 4">
+						<h2 class="title-slider-large">Free updates <br> Top notch Support</h2>
+					</div>
+
+					<div class="tp-caption sfl stl"
+						data-x="left" data-hoffset="15"
+						data-y="center" data-voffset="70" 
+						data-speed="700" data-start="1000"
+						data-endspeed="400" data-end="7900"
+						data-easing="Power3.easeInOut" data-endeasing=""
+						style="z-index: 4">
+						<h3 class="title-slider-small uppercased">and so much more ...</h3>
+					</div>
+
+
+					<div class="tp-caption sfb stl"
+						data-x="left" data-hoffset="15"
+						data-y="center" data-voffset="120" 
+						data-speed="900" data-start="1200"
+						data-endspeed="600" data-end="7950"
+						data-easing="Power3.easeInOut" data-endeasing=""
+						style="z-index: 4">
+						<a href="#" class="btn-slider reversed">see features</a>
+					</div>
+					
+				</li>
+
+
+				<!-- SLIDE 4 -->
+				<li data-transition="fade" data-slotamount="7" data-delay="8000" data-title="Slide 4">
+					<!-- Background Image -->
+				   <img src="images/carousel/slide_4.jpg" data-bgposition="center top" data-bgfit="cover" data-bgrepeat="no-repeat" alt="">
+				   <!-- <div class="bg-overlay op5"></div> -->
+
+				   
+					<div class="tp-caption sfr stb"
+						data-x="right" data-hoffset="-15"
+						data-y="center" data-voffset="0" 
+						data-speed="900" data-start="600"
+						data-endspeed="400" data-end="7980"
+						data-easing="Power3.easeInOut" data-endeasing=""
+						style="z-index: 4">
+						<h2 class="title-slider-large text-right">Premium Plugins</h2>
+					</div>
+
+					<div class="tp-caption sfr stb"
+						data-x="right" data-hoffset="-15"
+						data-y="center" data-voffset="70" 
+						data-speed="1000" data-start="1000"
+						data-endspeed="400" data-end="7900"
+						data-easing="Power3.easeInOut" data-endeasing=""
+						style="z-index: 4">
+						<h4 class="title-slider-small uppercased text-right">
+						Bootstrap based <span class="py10"></span>
+						responsive design <span class="py10"></span>
+						clean code<br>
+						working widgets  <span class="py10"></span>
+						ajax contact form
+						</h4>
+					</div>
+
+				</li>
+
+			</ul>
+		</div>
+	</div>
+	
+	<section class="section mt20">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-7 xs-box2">
+					<h2 class="title-medium br-bottom">Our Info</h2>
+					<p>If you have a dream of gardening, if you are a fond of collecting valuable and ornamental plants for your premises, we will provide you the best budget solution, keeping and maintaining the outstanding qualities and consultancy services. Our professionals are experienced to guide you the best consultancy services and proper A to Z solution for your nursery establishment, maintenance and activities.</p>
+					<p>Our wide and enormous variety of plant as well as seed collection will decorate your dream premises.</p>
+					<p><b>Our website always remains updated with the current availability.</b></p>
+					<div class="mb40"></div>
+				</div>
+				<div class="col-sm-5">
+					<div class="owl-carousel owl-portfolio mt10">
+						<div class="owl-el"><img src="images/carousel/our-info-slide_1.jpg" alt="blog image"></div>
+						<div class="owl-el"><img src="images/carousel/our-info-slide_2.jpg" alt="blog image"></div>
+						<div class="owl-el"><img src="images/carousel/our-info-slide_3.jpg" alt="blog image"></div>
+						<div class="owl-el"><img src="images/carousel/our-info-slide_4.jpg" alt="blog image"></div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	
+	<section class="section">
+		<div class="row col-p0 max_height sm_max_height">
+			<div class="col-sm-12 col-md-4">
+				<div class="box-services-d bg-img indoor el_max_height">
+					<div class="bg-overlay"></div>
+					<div class="row col-p0">
+						<div class="col-sm-12">
+							<h3 class="title-uppercased br-bottom">Indoor Plants <a href="indoor-plants.php" title="Indoor Plants" class="link-read-more">read more</a></h3>
+							<p class="mb0 ">It’s time to give new life to your interior. Indoor plant is a plant that is grown indoors in places such as residences and offices. Indoor plants not only help clean the environment around them</p>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-sm-12 col-md-4">
+				<div class="box-services-d bg-img flowering el_max_height">
+					<div class="bg-overlay"></div>
+					<div class="row col-p0">
+						<div class="col-sm-12">
+							<h3 class="title-uppercased br-bottom">Flowering Plants <a href="flowering-plants.php" title="Flowering Plants" class="link-read-more">read more</a></h3>
+							<p class="mb0 ">Flowering plants are such type of plants, which have many verities plants of flowers. Flower plants are known as garden plants. These plants are growing by seeds, so these plants are called seed growing plants.</p>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-sm-12 col-md-4">
+				<div class="box-services-d bg-img decorative el_max_height">
+					<div class="bg-overlay"></div>
+					<div class="row col-p0">
+						<div class="col-sm-12">
+							<h3 class="title-uppercased br-bottom">Decorative Plants <a href="decorative-plants.php" title="Decorative Plants" class="link-read-more">read more</a></h3>
+							<p class="mb0 ">We offer decorative plants that fit any style and budget and also beautify your home with the display of aesthetic features including: flowers, leaves, scent, overall foliage texture</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+
+	
+	<!-- Javascript files -->
+
+	<script src="plugins/jquery/jquery-2.1.0.min.js"></script>
+
+	<script src="plugins/bootstrap/js/bootstrap.min.js"></script>
+
+	<script src="plugins/rs-plugin/js/jquery.themepunch.tools.min.js"></script>
+
+	<script src="plugins/rs-plugin/js/jquery.themepunch.revolution.min.js"></script>
+
+	<script src="plugins/jquery.appear.js"></script>
+
+	<script src="plugins/retina.min.js"></script>
+
+	<script src="plugins/stellar.min.js"></script>
+
+	<script src="plugins/sticky.min.js"></script>
+
+	<script src="plugins/owl-carousel/owl.carousel.min.js"></script>
+
+	<!-- Main javascript file -->
+
+	<script src="js/script.js"></script>
+	
+	<!--Start of Tawk.to Script-->
+	<script type="text/javascript">
+	var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+	(function(){
+	var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+	s1.async=true;
+	s1.src='https://embed.tawk.to/58c4064d41acfb239f8a936b/defa
+	s1.charset='UTF-8';
+	s1.setAttribute('crossorigin','*');
+	s0.parentNode.insertBefore(s1,s0);
+	})();
+	</script>
+	<!--End of Tawk.to Script-->
+	
+</body>
+
+<!--
+Tempplates used: index_03.html
+
+
+-->
+
+</html>
---
+++ WebContent/bootstrap/indoor-plants.php
@@ -0,0 +1,321 @@
+<!DOCTYPE html>
+
+<html lang="en-us">
+
+<head>
+	<title>Mangalore Nursery, Mangalore - Indoor and Fruit Plants, Seeds Supplier</title>
+	
+	<meta name="author" content="Mangalore Nursery">
+	
+	<meta name="description" content="If you have a dream of gardening, if you are a fond of collecting valuable and ornamental plants for your premises, we will provide you the best budget solution, keeping and maintaining the outstanding qualities and consultancy services.">
+	
+	<meta name="keywords" content="Indian Plants, Flowering Plants, Fruit Plants, Hanging Plants, Herbal Plants, Indoor Plants, Palm Plants, Aquatic Plants, Cactus Plants">
+
+	<meta charset="utf-8" >
+
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<title>Aus </title>
+	
+	<!-- CSS files -->
+
+	<link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:100,300,400,600,700,900,400italic|Montserrat:400,700' rel='stylesheet'>
+
+	<link rel="stylesheet" href="plugins/bootstrap/css/bootstrap.min.css">
+
+	<link rel="stylesheet" href="plugins/font-awesome/css/font-awesome.min.css">
+
+	<link rel="stylesheet" href="plugins/elegant_font/html_css/style.css">
+
+	<link rel="stylesheet" href="plugins/rs-plugin/css/settings.css" media="screen">
+
+	<link rel="stylesheet" href="plugins/owl-carousel/owl.carousel.css">
+
+	<!-- Main CSS file -->
+
+	<link rel="stylesheet" href="css/style.css">
+	
+</head>
+
+<body>
+	
+	<div id="preloader">
+		<div id="status">&nbsp;</div>
+	</div>
+	
+	
+	<!-- Global Wrapper -->
+	<div id="wrapper">
+	
+		<!-- Add gray class to make the topbar light -->
+		<!-- Top Bar -->
+		<div class="topbar gray">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-6 hidden-xs">
+						<ul class="social-icon unstyled">
+							<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+							<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+							<li><a href="#"><i class="fa fa-instagram"></i></a></li>
+						</ul>
+					</div>
+					<div class="col-sm-6">
+						<div class="pull-right">
+							<ul class="top-menu">
+								<li><a href="#"><i class="fa fa-lock"></i>  Sign In</a></li>
+								<li><span class="py10"> | </span></li>
+								<li><a href="#"><i class="fa fa-shopping-cart"></i> Cart <span class="cart-items"></span></a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+	
+		<!-- Header -->
+		<header class="header-wrapper">
+			<div class="main-header">
+				<div class="container">
+					<div class="row">
+						<div class="col-sm-12 col-md-2">
+							<!-- Logo - Read documentation to see how to change the logo-->
+							<a href="index.php" class="logo"></a>
+						</div>
+						<div class="col-sm-12 col-md-10">
+							<nav class="navbar-left">
+								<!-- Added menu-light class to make the menu white -->
+								<ul class="menu menu-light">
+									<!-- Toggle Menu - For Mobile Devices -->
+									<li class="toggle-menu xs-toggle"><i class="fa icon_menu"></i></li>
+									
+									<li class="first">
+										<a href="#" class="first"><i class="fa fa-home menu-icon"></i> Home</a>
+									</li> <!-- END Home -->
+									<li>
+										<a href="#">About Us</a>
+									</li> <!-- END About Us -->
+									<li>
+										<a href="#">Services</a>
+									</li> <!-- END Services -->
+									<li>
+										<a href="#">Profile</a>
+									</li> <!-- END Profile -->
+									<li>
+										<a href="#">Offer</a>
+									</li> <!-- END Offer -->
+									<li>
+										<a href="#">Contact Us</a>
+									</li> <!-- END Contact Us -->									
+								</ul>
+							</nav>
+						</div>
+					</div>
+				</div> <!-- END Container -->
+			</div> <!-- END Main Header -->
+		</header>
+	</div>
+
+	<div class="tp-banner-container rs_fullwidth">
+		<div class="tp-banner">
+			<ul>
+				
+				<!-- SLIDE 1 -->
+				<li data-transition="fade" data-masterspeed="500" data-slotamount="7" data-delay="8000" data-title="Slide 1">
+					<!-- Background Image -->
+				   <img src="images/carousel/slide_1.jpg" data-bgposition="center top" data-bgfit="cover" data-bgrepeat="no-repeat" alt="">
+				   <!-- <div class="bg-overlay op6"></div> -->
+
+
+					<div class="tp-caption sfb stt"
+						data-x="left" data-hoffset="15"
+						data-y="center" data-voffset="-75" 
+						data-speed="600" data-start="600"
+						data-endspeed="400" data-end="7900"
+						data-easing="Power3.easeInOut" data-endeasing=""
+						style="z-index: 4">
+						<h3 class="title-slider-small uppercased mb10">create stunning websites with palas</h3>
+					</div>
+
+					<div class="tp-caption sfb stt"
+						data-x="left" data-hoffset="15"
+						data-y="center" data-voffset="25" 
+						data-speed="700" data-start="800"
+						data-endspeed="500" data-end="7950"
+						data-easing="Power3.easeInOut" data-endeasing=""
+						style="z-index: 4">
+						<h2 class="title-slider-large">All you need<br> just one template</h2>
+					</div>
+
+					<div class="tp-caption sfb stt"
+						data-x="left" data-hoffset="15"
+						data-y="center" data-voffset="130" 
+						data-speed="800" data-start="1000"
+						data-endspeed="600" data-end="8000"
+						data-easing="Power3.easeInOut" data-endeasing=""
+						style="z-index: 4">
+						<a href="#" class="btn-slider">Purchase</a>
+					</div>
+
+					
+				</li>
+
+				<!-- SLIDE 2 -->
+				<li data-transition="fade" data-slotamount="7" data-delay="8000" data-title="Slide 2">
+					<!-- Background Image -->
+				   <img src="images/carousel/slide_2.jpg" data-bgposition="center top" data-bgfit="cover" data-bgrepeat="no-repeat" alt="">
+				   <!-- <div class="bg-overlay op5"></div> -->
+
+				   
+					<div class="tp-caption sfl stt"
+						data-x="left" data-hoffset="15"
+						data-y="center" data-voffset="-10" 
+						data-speed="600" data-start="600"
+						data-endspeed="400" data-end="7800"
+						data-easing="Power3.easeInOut" data-endeasing=""
+						style="z-index: 4">
+						<h2 class="title-slider-large mb20">Customizable <br> and Easy to Use</h2>
+						<div class="br-bottom"></div>
+					</div>
+
+					<div class="tp-caption sfl stb"
+						data-x="left" data-hoffset="15"
+						data-y="center" data-voffset="110" 
+						data-speed="700" data-start="800"
+						data-endspeed="400" data-end="7900"
+						data-easing="Power3.easeInOut" data-endeasing=""
+						style="z-index: 4">
+						<h4 class="title-slider-small">It could take you a few minutes to customize it!</h4>
+					</div>
+					
+				</li>
+
+				<!-- SLIDE 3 -->
+				<li data-transition="fade" data-slotamount="7" data-delay="8000" data-title="Slide 3">
+					<!-- Background Image -->
+				   <img src="images/carousel/slide_3.jpg" data-bgposition="center top" data-bgfit="cover" data-bgrepeat="no-repeat" alt="">
+				   <!-- <div class="bg-overlay op5"></div> -->
+
+				   
+					<div class="tp-caption sfl stl"
+						data-x="left" data-hoffset="15"
+						data-y="center" data-voffset="-15" 
+						data-speed="700" data-start="600"
+						data-endspeed="400" data-end="7800"
+						data-easing="Power3.easeInOut" data-endeasing=""
+						style="z-index: 4">
+						<h2 class="title-slider-large">Free updates <br> Top notch Support</h2>
+					</div>
+
+					<div class="tp-caption sfl stl"
+						data-x="left" data-hoffset="15"
+						data-y="center" data-voffset="70" 
+						data-speed="700" data-start="1000"
+						data-endspeed="400" data-end="7900"
+						data-easing="Power3.easeInOut" data-endeasing=""
+						style="z-index: 4">
+						<h3 class="title-slider-small uppercased">and so much more ...</h3>
+					</div>
+
+
+					<div class="tp-caption sfb stl"
+						data-x="left" data-hoffset="15"
+						data-y="center" data-voffset="120" 
+						data-speed="900" data-start="1200"
+						data-endspeed="600" data-end="7950"
+						data-easing="Power3.easeInOut" data-endeasing=""
+						style="z-index: 4">
+						<a href="#" class="btn-slider reversed">see features</a>
+					</div>
+					
+				</li>
+
+
+				<!-- SLIDE 4 -->
+				<li data-transition="fade" data-slotamount="7" data-delay="8000" data-title="Slide 4">
+					<!-- Background Image -->
+				   <img src="images/carousel/slide_4.jpg" data-bgposition="center top" data-bgfit="cover" data-bgrepeat="no-repeat" alt="">
+				   <!-- <div class="bg-overlay op5"></div> -->
+
+				   
+					<div class="tp-caption sfr stb"
+						data-x="right" data-hoffset="-15"
+						data-y="center" data-voffset="0" 
+						data-speed="900" data-start="600"
+						data-endspeed="400" data-end="7980"
+						data-easing="Power3.easeInOut" data-endeasing=""
+						style="z-index: 4">
+						<h2 class="title-slider-large text-right">Premium Plugins</h2>
+					</div>
+
+					<div class="tp-caption sfr stb"
+						data-x="right" data-hoffset="-15"
+						data-y="center" data-voffset="70" 
+						data-speed="1000" data-start="1000"
+						data-endspeed="400" data-end="7900"
+						data-easing="Power3.easeInOut" data-endeasing=""
+						style="z-index: 4">
+						<h4 class="title-slider-small uppercased text-right">
+						Bootstrap based <span class="py10"></span>
+						responsive design <span class="py10"></span>
+						clean code<br>
+						working widgets  <span class="py10"></span>
+						ajax contact form
+						</h4>
+					</div>
+
+				</li>
+
+			</ul>
+		</div>
+	</div>
+	
+	<section class="section mt20">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-12 xs-box2">
+					<h2 class="title-medium br-bottom">Indoor Plants</h2>
+					<p><b>It's time to give new life to your interior.</b></p>
+					<p>Indoor plant is a plant that is grown indoors in places such as residences and offices. Indoor plants not only help clean the environment around them, but they are commonly grown for decorative purposes, positive psychological effects, or health reasons such as indoor air purification.</p>
+					<p>Indoor plants not only provide a nice aesthetic quality in the home, but they can have actual benefits on health. Many plants filter toxins like formaldehyde, xylene and toluene, which can seep into the home from building materials and paint. Beyond cleaning the air, plants can reduce stress and symptoms of depression. We have an exceptional range of quality indoor plants which sets us apart from other nurseries and our well trained & knowledgeable staffs provide you the specialized garden services. Major factors that should be considered when caring for houseplants are moisture, light, soil mixture, temperature, humidity, fertilizers, potting, and pest control. Our staff guidelines for houseplant care. For specific houseplant needs, the tags that sometimes come with plants are notoriously unhelpful and generic. Specific care information may be found widely online and in books.</p>
+					<div class="mb40"></div>
+				</div>
+			</div>
+		</div>
+	</section>	
+	
+	<!-- Javascript files -->
+
+	<script src="plugins/jquery/jquery-2.1.0.min.js"></script>
+
+	<script src="plugins/bootstrap/js/bootstrap.min.js"></script>
+
+	<script src="plugins/rs-plugin/js/jquery.themepunch.tools.min.js"></script>
+
+	<script src="plugins/rs-plugin/js/jquery.themepunch.revolution.min.js"></script>
+
+	<script src="plugins/jquery.appear.js"></script>
+
+	<script src="plugins/retina.min.js"></script>
+
+	<script src="plugins/stellar.min.js"></script>
+
+	<script src="plugins/sticky.min.js"></script>
+
+	<script src="plugins/owl-carousel/owl.carousel.min.js"></script>
+
+	<!-- Main javascript file -->
+
+	<script src="js/script.js"></script>
+	
+</body>
+
+<!--
+Tempplates used: index_03.html
+
+
+-->
+
+</html>	
